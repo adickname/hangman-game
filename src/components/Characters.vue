583a8d85be7c24ca4a53a20941dc5cf3a42b766d
Adrian Pasterka <you@example.com>
2024-03-21 18:16:01 +0100

    Add disabling button after click

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="container">
-        <button v-for="n in 26" @click="wordStore.setUserWord(String.fromCharCode(n + 64))">{{ String.fromCharCode(n + 64) }}</button>
+        <button v-for="n in 26" :key="n" @click="handleClick(n)" :disabled="isButtonClicked(n)">{{ String.fromCharCode(n + 64) }}</button>
     </div>
 </template>
 
 <script setup>
 import { useWordStore } from "../stores/WordStore.js"
+import {ref} from "vue"
+const clickedButton = ref([])
+function isButtonClicked(n){
+    return clickedButton.value.includes(n)
+}
+function handleClick(n){
+    clickedButton.value.push(n)
+    wordStore.setUserWord(String.fromCharCode(n + 64))
+}
 const wordStore = useWordStore()
 </script>
